6961f0600da772a3c9d7b5bd0a99f6dda2dfcafe
Alexis <villars.alexis@gmail.com>
2022-04-06 22:13:33 +0200

    commit 2

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -13,3 +13,27 @@ exports.getAllSauce = (req, res, next) => {
             });
         })
 };
+
+/* controleur de récupération une sauce */
+exports.getOnesauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then((sauces) => {
+        res.status(200).json(sauces);
+    }
+    ).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    }
+    );
+}
+
+
+
+
+
+
+
+
+
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,9 +2,13 @@
 const express = require("express");
 const router = express.Router();
 const sauceCtrl = require("../controllers/sauce");
+const auth = require('../middleware/auth');
+
 
 /* Routage User */
-router.get("/", sauceCtrl.getAllSauce);
+router.get("/", auth, sauceCtrl.getAllSauce);
+router.get("/", auth, sauceCtrl.getOnesauce);
+
 
 
 module.exports = router;
